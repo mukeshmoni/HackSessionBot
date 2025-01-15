01716b2cd21472b2088918ddf2cb4d6be66fb7f8
MUKESH BABU S <108213299+mukeshmoni@users.noreply.github.com>
2025-01-15 23:41:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = "20611365"
+    API_HASH = "424e183d2cf8a7f2c8bafeeaab1b5c8c"
+    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    TOKEN = "7573604459:AAGtbbye_5vZJb6Ezimlm-jJIQ2aogOQdEw"
+    START_PIC = "https://telegra.ph/file/0eba143d65f9413f9ae04.jpg"
+    CHAT = "https://t.me/+NiQ-XMDZ-89kOTRl"    
